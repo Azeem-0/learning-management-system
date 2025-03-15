5295fbbb68ef21d8a67ec8cc6d7308d7f04bc192
SATHVIK SVS <svssathvik77@gmail.com>
2025-03-15 13:35:49 +0530

    finish code contest

--- server/controllers/contest-controller/submission-controller.js
+++ server/controllers/contest-controller/submission-controller.js
@@ -58,26 +58,43 @@ const validateSubmission = async (req, res) => {
             }),
           }
         );
+        console.log(submissionResponse, "is the submission response");
+        // Get token from submission response
         const { token } = await submissionResponse.json();
+        if (!token) {
+          return res.status(400).json({
+            success: false,
+            message: "Submission failed due to busy server",
+          });
+        }
         const resultResponse = await fetch(
           `${JUDGE0_API_URL}/submissions/${token}?base64_encoded=false&fields=stdout,stderr,status_id,compile_output,time`,
           {
             method: "GET",
             headers: {
               "X-RapidAPI-Key":
-                "d048904afamsh7c58dce1604a4e9p176967jsna9a437cfb37f",
+                "d3aff5fa46msh58a825d2e6cca7cp1a3738jsn6aa52b7e64da",
               "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
             },
           }
         );
-
+        console.log(resultResponse, "is the ressss");
         const result = await resultResponse.json();
-        console.log(
-          result.stdout,
-          testCase.output,
-          result.stdout === testCase.output
-        );
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const normalizedOutput = result?.stdout?.trim();
+        const normalizedExpected = testCase?.output?.trim();
+        const passed = normalizedOutput === normalizedExpected;
+        results.push({
+          passed,
+          output: normalizedOutput,
+          expected: normalizedExpected,
+        });
+        allTestsPassed = allTestsPassed && passed;
+        console.log("Test case result:", {
+          output: normalizedOutput,
+          expected: normalizedExpected,
+          passed,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 5000));
       } catch (error) {
         console.error("Error processing test case:", error);
         results.push({
