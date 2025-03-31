53e45bba1eb5d2f2371db8c316e36ad44638c2d5
Azeem Shaik <azeemshaik025@gmail.com>
2025-03-31 11:34:58 +0530

    --amend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,28 +7,18 @@ import InstructorProvider from "./context/instructor-context/index.jsx";
 import StudentProvider from "./context/student-context/index.jsx";
 import NotificationProvider from "./context/notification-context/index.jsx";
 import Chatbot from "./components/chat-bot/index.jsx";
-import { WagmiProvider } from "wagmi";
-import { wagmiConfig } from "./web3/config/wagmiConfig";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 createRoot(document.getElementById("root")).render(
-  <WagmiProvider config={wagmiConfig}>
-    <QueryClientProvider client={new QueryClient()}>
-      <RainbowKitProvider showRecentTransactions={true}>
-        <BrowserRouter>
-          <NotificationProvider>
-            <AuthProvider>
-              <InstructorProvider>
-                <StudentProvider>
-                  <App />
-                  <Chatbot />
-                </StudentProvider>
-              </InstructorProvider>
-            </AuthProvider>
-          </NotificationProvider>
-        </BrowserRouter>
-      </RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
+  <BrowserRouter>
+    <NotificationProvider>
+      <AuthProvider>
+        <InstructorProvider>
+          <StudentProvider>
+            <App />
+            <Chatbot />
+          </StudentProvider>
+        </InstructorProvider>
+      </AuthProvider>
+    </NotificationProvider>
+  </BrowserRouter>
 );
